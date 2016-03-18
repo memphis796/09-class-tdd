49811f34ccc782aad0423f16723bfa6ecfd60b48
Memphis796 <memphis796@Hotmail.com>
2016-03-18 14:53:03 -0500

    tuesday homework assignment

--- src/classes.js
+++ src/classes.js
@@ -1,5 +1,68 @@
 class Foo {
 
 }
+var bar = new Foo;
 
-var bar;
+
+
+class Dog {
+   constructor() {
+     this.says = 'life is ruff';
+   }
+}
+
+
+class Cat {
+ growl() {
+   return 'meow';
+ }
+};
+
+var garfield = new Cat();
+
+
+
+class FacelessMan {
+ constructor(name) {
+   this.name = name;
+ }
+}
+var aMan = new FacelessMan(`Jaqen H'ghar`);
+
+
+
+class Lion extends Cat {
+ constructor(size) {
+    super();
+   this.size = 'BIG';
+ }
+}
+
+
+class KeepSecret {
+ constructor(secret) {
+   this.squeal = function () {
+     return secret;
+   }
+ }
+}
+
+
+
+
+class Key {
+contructor() {
+
+  }
+}
+
+class Safe {
+ constructor(secret, key) {
+   this.unlock = function (lockpick) {
+     if (lockpick == key) {
+       return secret;
+     }
+     return undefined;
+   }
+ }
+}
--- tests/classes-test.js
+++ tests/classes-test.js
@@ -27,7 +27,7 @@ test(`The order of the Faceless Men`, function(assert) {
 
   assert.ok(aMan instanceof FacelessMan, `Create a class called 'FacelessMan' and a new instance of FacelessMan called 'aMan'`);
 
-  assert.equal(aMan.name, `Jaqen H'ghar`, `A girl gives a man his own name (aMan.name should return 'Jaqen H'ghar')`);
+  assert.equal(aMan.name, `Jaqen H'ghar`, `A girl gives a man his own name (aMan.name should return )`);
 
   assert.equal(girl.name, `The Waif`,
     `The FacelessMan constructor takes an argument and sets a value called 'name' based on the passed in argument`);
